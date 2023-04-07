3d64f09657484d6e87570531d487047cfa95f1a7
Mateus Anderle da Silva <matsanderle@gmail.com>
2023-04-07 15:37:35 -0300

    FIrst commit

---
+++ src/components/HomeHeader.tsx
@@ -0,0 +1,31 @@
+import { HStack, Heading, Text, VStack, Icon } from "native-base";
+import { UserPhoto } from "./UserPhoto";
+import { MaterialIcons } from "@expo/vector-icons";
+import { TouchableOpacity } from "react-native";
+
+export function HomeHeader() {
+  return (
+    <HStack bgColor="gray.600" pt={16} pb={5} px={8} alignItems="center">
+      <UserPhoto
+        source={{ uri: "https://github.com/mateusanderle.png" }}
+        size={16}
+        alt="Imagem do usuário"
+        mr={4}
+      />
+
+      <VStack flex={1}>
+        <Text color="gray.100" fontSize="md">
+          Olá,
+        </Text>
+
+        <Heading color="gray.100" fontSize="md">
+          Mateus
+        </Heading>
+      </VStack>
+
+      <TouchableOpacity>
+        <Icon as={MaterialIcons} name="logout" size={7} />
+      </TouchableOpacity>
+    </HStack>
+  );
+}
